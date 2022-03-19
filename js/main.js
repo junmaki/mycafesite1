136c2ab8356b8e1bd30bb151084d4c5dd5ecd11c
junmaki <101034031+junmaki@users.noreply.github.com>
2022-03-19 16:38:47 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+{
+  // Intersection Observer API
+
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+}
