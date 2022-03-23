d7f1041fbeef8f3ebba2bbd3b1fc940087182671
junmaki <101034031+junmaki@users.noreply.github.com>
2022-03-24 02:34:17 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,64 @@
+'use strict';
+
+{
+  // Intersection Observer API
+
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if (currentIndex > images.length - 1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 3000);
+  }
+
+  const images = document.querySelectorAll('.hero img');
+  let currentIndex = 0;
+
+  play();
+
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+
+// クリックしたら消したい
+
+  document.querySelectorAll('a').forEach(e=>e.addEventListener('click', ()=>document.getElementById('nav-close').click()))
+
+}
